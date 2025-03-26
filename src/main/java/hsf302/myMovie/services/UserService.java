11c56809c8557d0fba68caf9d690bb0677b53145
caovanducanh <caovanducanh24012004@gmail.com>
2025-03-26 21:44:43 +0700

    admin user

--- src/main/java/hsf302/myMovie/services/UserService.java
+++ src/main/java/hsf302/myMovie/services/UserService.java
@@ -22,4 +22,38 @@ public class UserService {
     public User findByid(int id) {
         return userRepo.findById(id);
     }
+
+    public Object getAll() {
+        return userRepo.findAll();
+    }
+
+    public void deleteUserById(int id) {
+        userRepo.deleteById(id);
+    }
+
+    public User getUserById(int id) {
+        return userRepo.findById(id);
+    }
+
+    public void updateUser(int id, String userName, String fullName, String email, int role) {
+        User user = userRepo.findById(id);
+        if (user != null) {
+            user.setUserName(userName);
+            user.setFullName(fullName);
+            user.setEmail(email);
+            user.setRole(role);
+            userRepo.save(user);
+        }
+    }
+
+    public void addUser(String userName, String fullName, String email, String password, int role) {
+        User newUser = new User();
+        newUser.setUserName(userName);
+        newUser.setFullName(fullName);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        newUser.setRole(role);
+        userRepo.save(newUser);
+    }
+
 }
